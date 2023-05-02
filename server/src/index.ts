77865a8f0a3bb4b004cf87e7e04b20aef72874cb
JRSiwiecki <JRSiwiecki@gmail.com>
2023-05-02 16:59:31 -0700

    Connect MongoDB/Mongoose + add DeckSchema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 
 const app = express();
+const db = mongoose.connect(
+    "mongodb+srv://jrsiwiecki:" + process.env.PASSWORD + "@smartcards.eeafzig.mongodb.net/?retryWrites=true&w=majority"
+    );
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to Smartcards!");
