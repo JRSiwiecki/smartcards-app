f289099e9ddf9230ddb0fd7e5f218e371f7304b6
JRSiwiecki <JRSiwiecki@gmail.com>
2023-05-02 17:16:41 -0700

    Add POST request for creating new decks on DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,36 @@
+// Module Imports
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+require("dotenv").config();
 
+// Model Imports
+import Deck from "./models/Deck";
+
+// App Boilerplate
 const app = express();
-const db = mongoose.connect(
-    "mongodb+srv://jrsiwiecki:" + process.env.PASSWORD + "@smartcards.eeafzig.mongodb.net/?retryWrites=true&w=majority"
-    );
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to Smartcards!");
 });
 
-app.get("/hello", (req: Request, res: Response) => {
-    res.send("Hello, world!");
+app.post("/decks", async (req: Request, res: Response) => {
+    const newDeck = new Deck({
+        title: "Java Deck"
+    });
+
+    const createdDeck = await newDeck.save();
+    res.json(createdDeck);
 });
 
-app.listen(3000);
+mongoose.connect(
+    "mongodb+srv://jrsiwiecki:" + process.env.DB_PASSWORD + "@smartcards.eeafzig.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then( () => {
+        console.log(`Started on port ${PORT}`);
+        app.listen(PORT);
+    });
+
